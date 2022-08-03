e8c8be32fed15a4d36d8225d29d70eb506004004
HUYNHTHIAILINH <huynhthiailinh2105@gmail.com>
2022-08-03 13:59:20 +0700

    query parameter

--- src/index.js
+++ src/index.js
@@ -18,12 +18,17 @@ const port = 3000
 
 app.get("/", (req, res) => {
   res.render("home")
-});
+})
 
 app.get("/news", (req, res) => {
   res.render("news")
+})
+
+app.get("/search", (req, res) => {
+  console.log(req.query)
+  res.render("search")
 });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-});
+})
