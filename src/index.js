183167860bf0dbcf05099fbd628f076905cacab2
HUYNHTHIAILINH <huynhthiailinh2105@gmail.com>
2022-08-08 17:00:00 +0700

    Keep calm and commit

--- src/index.js
+++ src/index.js
@@ -1,25 +1,25 @@
-const path = require("path")
+const path = require('path');
 
-const express = require("express")
-const morgan = require("morgan")
-const handlebars = require("express-handlebars")
+const express = require('express');
+const morgan = require('morgan');
+const handlebars = require('express-handlebars');
 
-const app = express()
+const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')))
-app.use(morgan("combined"))
-app.use(express.urlencoded({extended: true}))
-app.use(express.json())
-app.engine("hbs", handlebars.engine({extname: ".hbs"}))
-app.set("view engine", "hbs")
-app.set("views", path.join(__dirname, "resources/views"))
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(morgan('combined'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.engine('hbs', handlebars.engine({ extname: '.hbs' }));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
 
-const route = require("./routes")
+const route = require('./routes');
 
-route(app)
+route(app);
 
-const port = 3000
+const port = 3000;
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Example app listening on port ${port}`);
+});
